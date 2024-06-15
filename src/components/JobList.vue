a2b32e3dce6ff75e6ed30f0d0699b7bc87e93ebc
gtopsis <csd3004@csd.uoc.gr>
2024-06-15 19:55:19 +0300

    feat: add components for Jobs list

---
+++ src/components/JobList.vue
@@ -0,0 +1,23 @@
+<script lang="ts" setup>
+import JobListItem from './JobListItem.vue'
+
+defineProps<{
+  jobs: any[]
+}>()
+</script>
+
+<template>
+  <ul class="w-full">
+    <li v-for="(job, index) in jobs" :key="index" class="mb-4 last:mb-0">
+      <JobListItem
+        :title="job[1]"
+        :url="job[4]"
+        :job-area="job[3]"
+        :location="job[2]"
+        :company="job[0]"
+      />
+    </li>
+  </ul>
+</template>
+
+<style lang="css" scoped></style>
---
+++ src/components/JobListItem.vue
@@ -0,0 +1,42 @@
+<script setup lang="ts">
+defineProps<{
+  title: string
+  url: string
+  jobArea: string
+  location: string
+  company: string
+}>()
+</script>
+
+<template>
+  <div class="flex items-center justify-between py-2 px-4 mb-4 bg-gray-100 shadow-sm rounded">
+    <div class="flex items-center gap-x-3">
+      <div>
+        <h6
+          class="block font-sans text-base font-semibold leading-relaxed tracking-normal text-gray-600 antialiased"
+        >
+          {{ title }}
+        </h6>
+        <p class="block font-sans text-sm font-light leading-normal text-gray-700 antialiased mb-2">
+          {{ company }}
+        </p>
+
+        <div class="grid grid-flow-col auto-cols-max md:auto-cols gap-2 pr-2">
+          <span
+            class="inline-flex items-center rounded-md bg-gray-200 px-2 py-1 text-xs font-medium text-gray-600 ring-1 ring-inset ring-gray-500/10"
+            >{{ jobArea }}</span
+          >
+          <span
+            class="inline-flex items-center rounded-md bg-gray-200 px-2 py-1 text-xs font-medium text-gray-600 ring-1 ring-inset ring-gray-500/10"
+            >{{ location }}</span
+          >
+        </div>
+      </div>
+    </div>
+    <h6
+      class="block font-sans text-base font-semibold leading-relaxed tracking-normal text-blue-900 antialiased"
+    >
+      <a :href="url" target="_blank" rel="noopener noreferrer">Read job ad</a>
+    </h6>
+  </div>
+</template>
