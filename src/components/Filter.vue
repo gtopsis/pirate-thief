0ac03a2d7c968ee499da8fea2b143d91f64a909d
gtopsis <csd3004@csd.uoc.gr>
2024-06-15 19:53:56 +0300

    feat: add components for filterσ

---
+++ src/components/Filter.vue
@@ -0,0 +1,35 @@
+<script lang="ts" setup>
+defineProps<{
+  name: string
+  active: boolean
+}>()
+</script>
+
+<template>
+  <div
+    data-dismissible="chip"
+    class="relative grid select-none items-center whitespace-nowrap rounded-lg bg-gray-900 py-1.5 px-3 font-sans text-xs font-bold uppercase text-white shadow-sm ring-1 ring-inset ring-gray-500/10 my-1"
+  >
+    <span class="mr-5">{{ name }}</span>
+    <button
+      data-dismissible-target="chip"
+      class="!absolute top-2/4 right-1 mx-px h-5 max-h-[32px] w-5 max-w-[32px] -translate-y-2/4 select-none rounded-md text-center align-middle font-sans text-xs font-medium uppercase text-white transition-all hover:bg-white/10 active:bg-white/30 disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none"
+      type="button"
+    >
+      <span class="absolute transform -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 24 24"
+          stroke="currentColor"
+          class="w-4 h-4"
+          stroke-width="2"
+        >
+          <path stroke-linecap="round" stroke-linejoin="round" d="M6 18L18 6M6 6l12 12"></path>
+        </svg>
+      </span>
+    </button>
+  </div>
+</template>
+
+<style lang="css" scoped></style>
---
+++ src/components/FilterList.vue
@@ -0,0 +1,20 @@
+<script lang="ts" setup>
+import Filter from './Filter.vue'
+import { computed } from 'vue'
+
+const props = defineProps<{
+  filters: Map<string, boolean>
+}>()
+
+const filtersEntries = computed(() => props.filters.entries())
+</script>
+
+<template>
+  <ul class="w-full flex justify-center flex-wrap">
+    <li v-for="[name, isEnabled] in filtersEntries" :key="name" class="mx-1 last:ml-0">
+      <Filter :name="name" :active="isEnabled" />
+    </li>
+  </ul>
+</template>
+
+<style lang="css" scoped></style>
