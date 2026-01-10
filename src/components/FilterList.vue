fd98ef7a7dbac93d5ae4eb69c1d44a1bbff489be
Giorgos Topsis <g.topsis@kaizengaming.com>
2026-01-10 13:05:38 +0200

    refactor(FilterList): simplify filter iteration by using props directly

--- src/components/FilterList.vue
+++ src/components/FilterList.vue
@@ -1,6 +1,5 @@
 <script lang="ts" setup>
 import FilterListItem from './FilterListItem.vue'
-import { computed } from 'vue'
 
 const props = defineProps<{
   filters: Map<string, boolean>
@@ -9,13 +8,11 @@ const props = defineProps<{
 const emit = defineEmits<{
   (e: 'filter:click', name: string): void
 }>()
-
-const filtersEntries = computed(() => props.filters.entries())
 </script>
 
 <template>
   <ul class="w-full h-full flex gap-2 flex-wrap justify-center">
-    <li v-for="[name, isEnabled] in filtersEntries" :key="name">
+    <li v-for="[name, isEnabled] in filters" :key="name">
       <FilterListItem :name="name" :active="isEnabled" @click="emit('filter:click', name)" />
     </li>
   </ul>
